acfa3b0f7cec2c5a998f4b2425db8c7df65909fb
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2025-08-22 04:37:19 +0200

    pulling nomic text embedding

--- worker/src/service/pinecone.ts
+++ worker/src/service/pinecone.ts
@@ -3,10 +3,17 @@ import { Vector } from "../types/job";
 
 const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
+  assistantRegion: process.env.PINECONE_REGION,
 });
 
-export async function upsertVectors(vectors: Vector[], namespace = "default") {
-  const index = pinecone.index(namespace);
+export async function upsertVectors(vectors: Vector[]) {
+  const indexName = process.env.PINECONE_INDEX_NAME;
+
+  if (!indexName) {
+    throw new Error("Pinecone index name is not set in environment variables.");
+  }
+
+  const index = pinecone.index(indexName);
 
   const records: PineconeRecord[] = vectors.map((vector) => ({
     id: vector.id,
