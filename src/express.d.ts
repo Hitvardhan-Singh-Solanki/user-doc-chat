a00fd4e38892c02678239d6386c16e10c3f2c260
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2025-09-14 04:44:00 +0200

    fixing declarations

--- src/express.d.ts
+++ src/express.d.ts
@@ -1,17 +1,11 @@
 import { JwtPayload } from 'jsonwebtoken';
+import type { Logger } from 'pino';
 
 declare global {
   namespace Express {
-    import type { Logger } from 'pino';
-    import { JwtPayload } from 'jsonwebtoken';
-
-    declare global {
-      namespace Express {
-        interface Request {
-          user?: JwtPayload | { [key: string]: any };
-          log: Logger;
-        }
-      }
+    interface Request {
+      user?: JwtPayload | { [key: string]: any };
+      log: Logger;
     }
   }
 }
