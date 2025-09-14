663fa4acee69db20dc40ea4abf3f275701a1fa0d
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2025-09-14 04:32:06 +0200

    fixing eslint

--- src/express.d.ts
+++ src/express.d.ts
@@ -2,16 +2,16 @@ import { JwtPayload } from 'jsonwebtoken';
 
 declare global {
   namespace Express {
-import type { Logger } from 'pino';
-import { JwtPayload } from 'jsonwebtoken';
+    import type { Logger } from 'pino';
+    import { JwtPayload } from 'jsonwebtoken';
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: JwtPayload | { [key: string]: any };
-      log: Logger;
+    declare global {
+      namespace Express {
+        interface Request {
+          user?: JwtPayload | { [key: string]: any };
+          log: Logger;
+        }
+      }
     }
-  }
-}
   }
 }
